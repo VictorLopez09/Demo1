f49074fc38483c1772a47d576cc46e38d8d688ea
VictorLopez09 <106134067+VictorLopez09@users.noreply.github.com>
2023-03-14 18:46:04 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <p>Hola mundo</p>
+
+
+    <?php
+    //Comentario
+    /*
+    Multilinea
+    */
+    echo 'Hola mundo desde php';
+    echo '<h1>Mezclando PHP con HTML</h1>';
+    echo '<a href="perfil.php">Mi perfil</a>';
+    ?>
+    <a href="perfil.php">Mi perfil</a>
+</body>
+</html>
---
+++ perfil.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi perfil</title>
+</head>
+<body>
+    <h2>
+        Nombre:
+    </h2>
+    <?php
+        echo "Victor Hugo lopez gomez";
+    ?>
+    <h2>
+        Edad:
+    </h2>
+    <?php
+        echo "20";
+    ?>
+    <h2>
+        Nivel academuico
+    </h2>
+
+    <?php
+
+    echo "Preparatoria";
+    ?>
+
+</body>
+</html>
